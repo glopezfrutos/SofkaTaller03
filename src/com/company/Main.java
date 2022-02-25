3b2c8a24e0291209a86ee2ce6268f64b6196c2a9
glopezfrutos <gus.lopezfrutos@gmail.com>
2022-02-25 12:00:42 -0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+// ToDo: Una biblioteca principal con todas las canciones
+
+// ToDo: Se debe tener una funcionalidad que permita crear playlist con canciones determinadas
+
+// ToDo: Se debe crear comportamientos en donde se permita hacer filtros a la biblioteca principal, los filtros son:
+//          TODO Filtrar canciones del mismo genero
+//          TODO Filtrar canciones en el mismo año
+
+// ToDo: Se debe crear comportamientos en donde se permita ordenar duración y fecha.
+//
+}
+}
---
+++ src/com/company/Song.java
@@ -0,0 +1,6 @@
+package com.company;
+
+public class Song {
+    // ToDo: Cada canción debe tener el titulo, identificador, fecha, duración, genero, carátula
+    //  y una descripción corta.
+}
